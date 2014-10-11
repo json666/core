1994e28845ff5c9012ce6b0e8540893425bd27d0
json666 <jheysonsanchez@gmail.com>
2014-10-11 13:54:05 +0200

    core

---
+++ src/main/java/com/bo/openlogic/core/bean/JsonResult.java
@@ -0,0 +1,48 @@
+package com.bo.openlogic.core.bean;
+
+/**
+ * Created by json on 7/09/14.
+ */
+
+public class JsonResult {
+    private Boolean success;
+    private String message;
+    private Object result;
+
+    public JsonResult(Boolean success, String message, Object result) {
+        this.success = success;
+        this.message = message;
+        this.result = result;
+    }
+
+    public JsonResult() {
+    }
+
+    public JsonResult(boolean b, String message) {
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+}
